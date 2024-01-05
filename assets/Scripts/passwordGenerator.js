a39f85b3f24631bd406d3cdae1d3dc3d93357219
Aiden <macuser@Overapt.local>
2024-01-05 01:18:51 -0700

    comments

--- assets/Scripts/passwordGenerator.js
+++ assets/Scripts/passwordGenerator.js
@@ -11,31 +11,31 @@ function generatePassword() {
     var numeric = "0123456789";
     var specialSymbols = "!\"#$%&()*+,-./:;<=>?@[\]^_`{|}~";
 
-//password variables for allowable password characters
-var passwordLength = prompt("Your password should have a character length of atleast 8 and no more than 128!");
-if (passwordLength < 8 || passwordLength > 128) {
-    alert("Incorrect password length! Try again.");
+    //password variables for allowable password characters
     var passwordLength = prompt("Your password should have a character length of atleast 8 and no more than 128!");
-}
-//Issue: this ternary operator function below creates an undefined pass.length
-// var passwordLength = (passwordLength < 8 || passwordLength > 128) ? (alert("Incorrect password length! Try again."), prompt("Your password should have a character length of atleast 8 and no more than 128!")):
-console.log("passwordLength: " + passwordLength);
+    if (passwordLength < 8 || passwordLength > 128) {
+        alert("Incorrect password length! Try again.");
+        var passwordLength = prompt("Your password should have a character length of atleast 8 and no more than 128!");
+    }
+    //Issue: this ternary operator function below creates an undefined pass.length ... used if - statement  above instead
+    // var passwordLength = (passwordLength < 8 || passwordLength > 128) ? (alert("Incorrect password length! Try again."), prompt("Your password should have a character length of atleast 8 and no more than 128!")):
+    console.log("passwordLength: " + passwordLength);
 
-// // Criteria prompts for password to generate
-let lowercaseConfirm = confirm("Your password should have a lowercase letter! Click OK to Continue") ? userChoice = userChoice.concat(lowercaseABC) : delete userChoice.lowercaseABC;
-let uppercaseConfirm = confirm("Your password should have an uppercase letter! Click OK to Continue") ? userChoice = userChoice.concat(uppercaseABC): delete userChoice.uppercaseABC;
-let symbolsConfirm = confirm("Your password should have a symbol! Click OK to Continue") ? userChoice = userChoice.concat(specialSymbols): delete userChoice.specialSymbols;
-let numbersConfirm = confirm("Your password should have a number! Click OK to Continue") ? userChoice = userChoice.concat(numeric): delete userChoice.numeric;
+    // prompts for password to generate
+    let lowercaseConfirm = confirm("Your password should have a lowercase letter! Click OK to Continue") ? userChoice = userChoice.concat(lowercaseABC) : delete userChoice.lowercaseABC;
+    let uppercaseConfirm = confirm("Your password should have an uppercase letter! Click OK to Continue") ? userChoice = userChoice.concat(uppercaseABC): delete userChoice.uppercaseABC;
+    let symbolsConfirm = confirm("Your password should have a symbol! Click OK to Continue") ? userChoice = userChoice.concat(specialSymbols): delete userChoice.specialSymbols;
+    let numbersConfirm = confirm("Your password should have a number! Click OK to Continue") ? userChoice = userChoice.concat(numeric): delete userChoice.numeric;
 
-//noCriteria function
-if ((!lowercaseConfirm && !uppercaseConfirm && !symbolsConfirm && !numbersConfirm) || !userChoice) {
-    alert("Must choose atleast one criteria!");
-    generatePassword();
-}
-// (!lowercaseConfirm && !uppercaseConfirm && !symbolsConfirm && !numbersConfirm) || (!userChoice) ? (alert("Must choose atleast one criteria!"), generatePassword()): userChoice;
+    // no Criteria function
+    if ((!lowercaseConfirm && !uppercaseConfirm && !symbolsConfirm && !numbersConfirm) || !userChoice) {
+        alert("Must choose atleast one criteria!");
+        generatePassword();
+    }
+    // (!lowercaseConfirm && !uppercaseConfirm && !symbolsConfirm && !numbersConfirm) || (!userChoice) ? (alert("Must choose atleast one criteria!"), generatePassword()): userChoice;
 
-console.log("userChoiceConditional: " + userChoice);
-console.log(passwordLength);
+    console.log("userChoiceConditional: " + userChoice);
+    console.log(passwordLength);
     let passwordEnd = "";
     for (let i = 0; i < passwordLength; i++) {
         let char = Math.floor(Math.random() * userChoice.length);
